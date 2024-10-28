3347c46c6fdabd3f824e81186080a36fb9a6b249
maksim25y <mudanov515@gmail.com>
2024-10-28 23:03:38 +0300

    made some refactoring in jwt service

--- src/main/kotlin/ru/mudan/service/secutiry/JwtService.kt
+++ src/main/kotlin/ru/mudan/service/secutiry/JwtService.kt
@@ -5,13 +5,8 @@ import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.io.Decoders
 import io.jsonwebtoken.security.Keys
 import lombok.RequiredArgsConstructor
-import org.hibernate.query.sqm.TemporalUnit
-import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.security.authentication.AuthenticationServiceException
 import org.springframework.security.core.userdetails.UserDetails
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
-import ru.mudan.domain.repository.RefreshTokenRepository
 import java.time.Duration
 import java.util.*
 import java.util.function.Function
@@ -22,9 +17,7 @@ import javax.crypto.SecretKey
 class JwtService(
     val jwtSecretKey: String,
     val tokenTTL: Duration,
-    @Qualifier("userDetailsService") private val userDetailsService: UserDetailsService,){
-    val refreshTTL = 86400000
-
+    val refreshTokenTTL: Duration){
     fun extractEmail(token: String?): String {
         return extractClaim(token) { obj: Claims -> obj.subject }
     }
@@ -47,24 +40,24 @@ class JwtService(
         extraClaims: Map<String?, Any?>?,
         userDetails: UserDetails
     ): String {
-        return Jwts.builder()
-            .claims(extraClaims)
-            .subject(userDetails.username)
-            .issuedAt(Date(System.currentTimeMillis()))
-            .expiration(Date(System.currentTimeMillis() + tokenTTL!!.toMillis()))
-            .signWith(signInKey, Jwts.SIG.HS256)
-            .compact()
+        return generateTokenByTTL(extraClaims,userDetails,tokenTTL)
     }
 
     fun generateRefreshToken(
         extraClaims: Map<String?, Any?>?,
         userDetails: UserDetails
     ): String {
+        return generateTokenByTTL(extraClaims,userDetails,refreshTokenTTL)
+    }
+
+    private fun generateTokenByTTL(extraClaims: Map<String?, Any?>?,
+                                   userDetails: UserDetails,
+                                   ttl:Duration): String {
         return Jwts.builder()
             .claims(extraClaims)
             .subject(userDetails.username)
             .issuedAt(Date(System.currentTimeMillis()))
-            .expiration(Date(System.currentTimeMillis() + refreshTTL))
+            .expiration(Date(System.currentTimeMillis() + ttl.toMillis()))
             .signWith(signInKey, Jwts.SIG.HS256)
             .compact()
     }
